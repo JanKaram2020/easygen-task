8c9835b76f20d2a42ae577c5d42b9d65efccd0f0
jan <jankarm2020@gmail.com>
2025-03-20 20:31:22 +0200

    init mongoose and remove PNPM

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
 import { LinksModule } from './links/links.module';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import {ConfigModule} from "@nestjs/config";
+
+const connectionString = process.env.DB_CONNECTION ?? '';
+console.log('MongoDB connectionString',connectionString);
 
 @Module({
-  imports: [LinksModule],
+  imports: [ConfigModule.forRoot(),MongooseModule.forRoot(connectionString),LinksModule],
   controllers: [AppController],
   providers: [AppService],
 })
